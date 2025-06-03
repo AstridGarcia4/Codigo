1438291f10d1210ea29bd4b486fab0e3f3f808e8
AstridGarcia4 <astridxgarcia4@gmail.com>
2025-06-03 09:23:53 -0600

    ahorrodel3.7.cpp

--- ahorrodel3.7.cpp
+++ ahorrodel3.7.cpp
@@ -1,9 +1,11 @@
+//calcula la cantidad ahorrada mensual de 15000
+//Astrid Alejandra Bustos Garcia
 #include <iostream>
 #include <iomanip>  // Para usar fixed y setprecision
 using namespace std;
 
 int main() {
-    double depositoMensual = 15000.0;        // Depósito mensual en pesos
+    double depositoMensual = 15000.0;        // DepÃ³sito mensual en pesos
     double tasaInteresMensual = 0.037;       // 3.7% mensual
     int anios = 15;
     int meses = anios * 12;
@@ -14,7 +16,7 @@ int main() {
     }
 
     cout << fixed << setprecision(10); // Mostrar hasta 10 decimales
-    cout << "Cantidad ahorrada después de " << anios << " años: $" << totalAhorrado << endl;
+    cout << "Cantidad ahorrada despuÃ©s de " << anios << " aÃ±os: $" << totalAhorrado << endl;
 
     return 0;
 }
